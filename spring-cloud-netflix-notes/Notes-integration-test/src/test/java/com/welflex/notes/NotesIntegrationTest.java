f7d44b190502b642c8393478754984b54f507589
Sanjay Acharya <sanjayvacharya@gmail.com>
2015-11-02 21:08:17 -0700

    Formatting and use of WebIntegrationTest.

--- spring-cloud-netflix-notes/Notes-integration-test/src/test/java/com/welflex/notes/NotesIntegrationTest.java
+++ spring-cloud-netflix-notes/Notes-integration-test/src/test/java/com/welflex/notes/NotesIntegrationTest.java
@@ -11,11 +11,9 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.inject.Inject;
 
