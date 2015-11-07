08944f5791c01a73e6dd872bff040f4db6881cc8
Sanjay Acharya <sanjayvacharya@gmail.com>
2015-11-06 18:17:55 -0700

    Removed unused import

--- spring-cloud-netflix-notes/Notes-webapp/src/main/java/com/welflex/spring/web/notes/Application.java
+++ spring-cloud-netflix-notes/Notes-webapp/src/main/java/com/welflex/spring/web/notes/Application.java
@@ -1,11 +1,9 @@
 package com.welflex.spring.web.notes;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
