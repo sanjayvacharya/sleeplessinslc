0af13ddb39658bc327c87d99cff43582980ed0ec
Sanjay Acharya <sanjayvacharya@gmail.com>
2015-12-24 07:06:26 -0700

    Removed un-used imports

--- spring-cloud-netflix-notes/Notes-webapp/src/test/java/NotesControllerTest.java
+++ spring-cloud-netflix-notes/Notes-webapp/src/test/java/NotesControllerTest.java
@@ -1,27 +1,18 @@
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.internal.mapper.ObjectMapperType;
 import com.welflex.spring.web.notes.Application;
 import com.welflex.spring.web.notes.Note;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
 
-import javax.inject.Inject;
-import javax.ws.rs.core.MediaType;
-
-import static com.jayway.restassured.RestAssured.baseURI;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
-import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -41,7 +32,7 @@ public class NotesControllerTest {
 
   @Test
   public void createAndGetNote() {
-    given().contentType(MediaType.APPLICATION_XML)
+    given().contentType(MediaType.APPLICATION_XML_VALUE)
            .body(new Note("Test"))
             .when()
             .post("http://localhost:" + port + "/notes")
